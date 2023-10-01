f204f5ddb3a1ccfda4288b5845ce10d3989403a1
Dustin Clark <dclark2434@gmail.com>
2023-09-30 23:21:43 -0500

    added pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,4 +5,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Show all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
